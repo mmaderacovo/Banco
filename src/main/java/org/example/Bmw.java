163e53a10f3143fb0427bb1d1a54ba4f7465331e
Angelica <mmaderacovo@gmail.com>
2023-02-20 16:44:59 -0500

    Poliorfismo

---
+++ src/main/java/org/example/Bmw.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public class Bmw extends Auto{
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-       Banco bankItau = new Banco("Itau");
+       /*Banco bankItau = new Banco("Itau");
        Usuario juanPablo = new Usuario("Juan Pablo",1098, 0);
        Usuario mariaAngelica = new Usuario("Maria Angelica", 1095,0);
 
@@ -24,11 +24,13 @@ public class Main {
         Banco.inicialMoney = 3000;
         System.out.println("Dinero comun " + Banco.inicialMoney);
         System.out.println("Nombre del Banco " + bankItau.getName());
-        System.out.println("Dinero disponible = " + bankItau.getMoney());
+        System.out.println("Dinero disponible = " + bankItau.getMoney());*/
 
         //Ejemplo override
-        Renault renault = new Renault();
+        Auto renault = new Renault();
+        Auto bmw = new Bmw();
         renault.pitar();
+        bmw.pitar();
 
     }
 }
